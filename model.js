089c110ed77857a64779ce4428d8489d60266e1f
halvaradop_ <hernanvid123@gmail.com>
2023-03-25 12:38:30 -0500

    get user data put in of the form

---
+++ model.js
@@ -0,0 +1,8 @@
+
+
+
+document.addEventListener('submit', event => {
+    event.preventDefault()
+    const form = document.getElementById('formDataElement')
+    const formData = new FormData(form)
+})
